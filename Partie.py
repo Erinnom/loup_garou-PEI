a185e455c5fd9b32a9740314ae1733d09b02fcbc
Merlin <laudy.merlin@gmail.com>
2024-10-09 16:14:36 +0200

    add,edit:Partie.py
    Ajout du squelette de la classe Partie

---
+++ Partie.py
@@ -0,0 +1,28 @@
+class Partie():
+    def __init__(self):
+        self.id_partie = ""
+
+
+    def creer(self, id_partie : str, nbr_joueur : int):
+        """Méthode permettant de créer une nouvelle partie en spécifiant
+            son identifiant et permettant la saisie des joueurs et leur attribution
+             des rôles"""
+        pass
+
+    def sauvegarder(self, nom_fichier: str):
+        """Méthode permettant de sauvegarder la partie en cours
+            dans un document Json"""
+        pass
+
+    def charger(self, id_partie: str):
+        """Méthode permettant de charger un fichier Json pour
+            reprendre la partie là où elle c'est arrêté"""
+        pass
+
+    def tour(self):
+        """Méthode qui effectue tout un tour de jeu"""
+        pass
+
+if __name__ == "__main__":
+    test = Partie("Partie_test")
+    help(Partie.creer)
